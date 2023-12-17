9014f2cd6444bb7bca1fc323a6df755e99685783
baigyth <baigyth@gmail.com>
2023-12-17 18:03:23 +0800

    reader/dailyBarReader读取日线文件.day

--- test/testReader.js
+++ test/testReader.js
@@ -1,7 +1,11 @@
 const path = require('path');
 const TdxMinuteBarReader = require('../reader/minuteBarReader');
+const TdxDailyBarReader = require('../reader/dailyBarReader');
 
 const reader = new TdxMinuteBarReader();
+const dayReader = new TdxDailyBarReader();
 
 const result = reader.parseDataFromFile(path.join(__dirname, './sz000001.lc1'));
+const dayResult = dayReader.parseDataFromFile(path.join(__dirname, './bj872895.day'));
 console.log(JSON.stringify(result));
+console.log(JSON.stringify(dayResult));
